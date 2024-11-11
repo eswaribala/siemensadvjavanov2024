860fc92433c67d7d784bc19e45164761529b6b00
Parameswari Bala <parameswaribala@gmail.com>
2024-11-11 10:51:25 +0530

    updated

---
+++ powerplantapp/src/main/java/com/siemens/Main.java
@@ -0,0 +1,25 @@
+package com.siemens;
+
+public class Main {
+    public static void main(String[] args) {
+        String name="Parameswari";
+        //runnable
+       Runnable runnable=()->{
+          for(char ch : name.toCharArray())
+          {
+              System.out.print(ch);
+              try {
+                  Thread.sleep(1500);
+              } catch (InterruptedException e) {
+                  throw new RuntimeException(e);
+              }
+          }
+
+       } ;
+
+       Thread thread=new Thread(runnable);
+       thread.start();
+
+
+    }
+}
