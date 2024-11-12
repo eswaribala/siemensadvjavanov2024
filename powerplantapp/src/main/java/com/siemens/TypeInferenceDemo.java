e05a261711fd20f9617141c1b40fd5cddf814a8d
Parameswari Bala <parameswaribala@gmail.com>
2024-11-12 10:59:36 +0530

    updated

--- powerplantapp/src/main/java/com/siemens/TypeInferenceDemo.java
+++ powerplantapp/src/main/java/com/siemens/TypeInferenceDemo.java
@@ -1,13 +1,27 @@
 package com.siemens;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TypeInferenceDemo {
 
     public static void main(String[] args){
         var var="var";
         var value=76;
+        var height=157.5;
 
         System.out.println(var.getClass().getName());
-      
-       System.out.println(((Object)value).getClass().getSimpleName());
+        System.out.println(((Object)value).getClass().getSimpleName());
+        System.out.println(((Object)height).getClass().getSimpleName());
+
+      List<String> routerNames=StreamDemo.generateRouters().stream().map(r->r.getRouterName()).collect(Collectors.toList());
+      routerNames.add(null);
+      routerNames.stream()
+              .filter(name->name!=null)
+              .map((@NotNull var name)->name.toUpperCase())
+              .forEach(System.out::println);
     }
 }
