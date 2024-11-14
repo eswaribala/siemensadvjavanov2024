ad6ec31d91571686986311d1656df7e119527b76
Parameswari Bala <parameswaribala@gmail.com>
2024-11-14 15:44:28 +0530

    updated

---
+++ cloudapigateway/src/main/java/com/siemens/cloudapigateway/CloudapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.cloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudapigatewayApplication.class, args);
+    }
+
+}
