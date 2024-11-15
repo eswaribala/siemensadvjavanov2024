faa201eb4d81def273633ca3ba56e2fd42d2fd7f
Parameswari Bala <parameswaribala@gmail.com>
2024-11-15 16:43:44 +0530

    updated

--- registrationapi/src/main/java/com/siemens/registrationapi/RegistrationapiApplication.java
+++ registrationapi/src/main/java/com/siemens/registrationapi/RegistrationapiApplication.java
@@ -2,8 +2,11 @@ package com.siemens.registrationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RegistrationapiApplication {
 
     public static void main(String[] args) {
