e1caef0a6f4de8591f6dba4a1e53cdbaeb6f0e27
Parameswari Bala <parameswaribala@gmail.com>
2024-11-15 10:34:35 +0530

    updated

--- circuitbreakerdemo/src/main/java/com/siemens/circuitbreakerdemo/services/CBService.java
+++ circuitbreakerdemo/src/main/java/com/siemens/circuitbreakerdemo/services/CBService.java
@@ -3,13 +3,14 @@ package com.siemens.circuitbreakerdemo.services;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
-
+@Slf4j
 public class CBService {
     @Autowired
     private RestClient restClient;
@@ -23,6 +24,7 @@ public class CBService {
     @Retry(name = "gatewayRetry")
     @RateLimiter(name="gatewayRateLimiter")
     public String getUserAccounts(){
+        log.info("Entering into Accounts API call...");
        return restClient
                .get()
                .uri(serviceUrl)
@@ -32,6 +34,7 @@ public class CBService {
     }
 
     public String fallback(Exception e){
+        log.info("Entering into Fallback Accounts API call...");
         return restClient
                 .get()
                 .uri(fallbackUrl)
